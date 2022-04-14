e5796c4cb8b9951b4a7c51035ba8cf9d5424c52b
Carrie Yu <carrieyu67@gmail.com>
2022-04-15 03:06:08 +0800

    registration update

--- server.js
+++ server.js
@@ -50,6 +50,31 @@ app.post('/api/user/login', function(req, res){
     }
 })
 
+app.post('/api/user/registration', function(req, res) {
+    console.log(req.body);
+    let firstname = req.body.firstname;
+    let lastname = req.body.lastname;
+    let phonenum = req.body.phonenum;
+    let addr = req.body.address;
+    let username = req.body.username;
+    let password = req.body.password;
+    let usertype = 'Customer';
+    let status = 1;
+    if(firstname && lastname && phonenum && addr && username && password){
+        conn.query('INSERT INTO pack_customer (cust_name, cust_lastname, cust_phonenum, cust_address, username, password, user_type, status) VALUES(?,?,?,?,?,?,?,?)',
+        [firstname, lastname, phonenum, addr, username, password, usertype, status], function(error, results, fields) {
+            if(error) throw error;
+            else{
+                res.send(results)
+                console.log(results);
+            }
+        })
+    }else{
+        res.send('Please Input the needed fields');
+        res.end();
+    }
+})
+
 app.get('/api/retrieve/services', (req, res) => {
     conn.query('SELECT *, pack_service.id as id, pack_caterer.id as cat_id FROM pack_service INNER JOIN pack_caterer on pack_service.pack_caterer_id = pack_caterer.id', function(error, rows, fields){
         if(error) console.log(error);
